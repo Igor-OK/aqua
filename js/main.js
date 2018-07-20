e6266b8dad0885c82f4ea91de0a0d04066f17c77
Igor Sanko <ig.sanko@gmail.com>
2018-07-20 17:20:40 +0300

    animated gradient

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 	var pinIntroScene = new ScrollMagic.Scene({
 		triggerElement: '.header',
 		triggerHook: 0,
-		duration: '230%'  // столько крутит до начала движения
+		duration: '200%'  // столько крутит до начала движения
 	})
 	.setPin('.header',{pushFollowers: false})
 	.addTo(controller);
@@ -15,7 +15,7 @@ $(document).ready(function(){
 	var pinIntroScene2 = new ScrollMagic.Scene({
 		triggerElement: '.header',
 		triggerHook: 0,
-		duration: '230%'  // столько крутит до начала движения
+		duration: '200%'  // столько крутит до начала движения
 	})
 	.setPin('.main-text',{pushFollowers: false})
 	.addTo(controller);
@@ -23,12 +23,23 @@ $(document).ready(function(){
 	var pinIntroScene3 = new ScrollMagic.Scene({
 		triggerElement: '.header',
 		triggerHook: 0,
-		duration: '230%'  // столько крутит до начала движения
+		duration: '200%'  // столько крутит до начала движения
 	})
 	.setPin('.courses',{pushFollowers: false})
 	.addTo(controller);
 
-	//================tween scene scrollmagic (works)
+	//================ Starting tween scene scrollmagic (already works)
+
+	var tweenHandyText = TweenMax.to('.start-animation', 1, {transform:'scale(1)', ease: Linear.easeNone});
+	var whiteUnderWear = new ScrollMagic.Scene({
+		offset: 0, //отступ
+		duration: 350 // продолжение анимации
+	})
+	.setTween(tweenHandyText)
+	.addIndicators(true)
+	.addTo(controller)
+
+
 	var tweenWhite = TweenMax.to('.under-animation', 1, {opacity:0, ease: Linear.easeNone});
 	var whiteUnderWear = new ScrollMagic.Scene({
 		offset: 200, //отступ
@@ -41,7 +52,7 @@ $(document).ready(function(){
 
 	var tweenGirl = TweenMax.to('.water-colour-gif', 1, {opacity:1, ease: Linear.easeNone});
 	var whiteUnderWear = new ScrollMagic.Scene({
-		offset: 800, //отступ
+		offset: 555, //отступ
 		duration: 300 // продолжение анимации
 	})
 	.setTween(tweenGirl)
@@ -70,15 +81,34 @@ $(document).ready(function(){
 	});
 
 
-});
 
-	// var underRule = CSSRulePlugin.getRule(".under");
+	//animated gradient in canvas
+	var granimInstance = new Granim({
+    element: '#canvas-basic',
+	    name: 'basic-gradient',
+	    direction: 'left-right',
+	    opacity: [1, 1],
+	    isPausedWhenNotInView: true,
+	    states : {
+	        "default-state": {
+	            gradients: [
+	                ['#AA076B', '#61045F'],
+	                ['#02AAB0', '#00CDAC'],
+	                ['#DA22FF', '#9733EE']
+	            ]
+	        }
+	    }
+	});
 
-	// TweenMax.to(underRule, 3, {
-	// 	cssRule: {
-	// 		backgroundColor: "yellow"
-	// 	},
-	// 	ease: Linear.easeNone,
-	// 	repeat: -1,
-	// 	yoyo: true
-	// });
+	// resize canvas for according content (.main text)
+	var gradientCanvas = document.getElementById('canvas-basic');
+	var mainText = document.querySelector('.main-text-content');
+	gradientCanvas.style.height = mainText.offsetHeight+120+'px';
+
+
+
+
+
+}); // end of document.ready
+
+
