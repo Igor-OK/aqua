0a321597aaead1970c0aa2d8235e9b2b9ef0b47c
Igor Sanko <ig.sanko@gmail.com>
2018-07-30 23:03:10 +0300

    teachers section added

--- js/main.js
+++ js/main.js
@@ -52,7 +52,7 @@ $(document).ready(function(){
 		duration: 350 // продолжение анимации
 	})
 	.setTween(tweenHandyText)
-	.addIndicators(true)
+	// .addIndicators(true)
 	.addTo(controller)
 
 
@@ -62,7 +62,7 @@ $(document).ready(function(){
 		duration: 400 // продолжение анимации
 	})
 	.setTween(tweenWhite)
-	.addIndicators(true)
+	// .addIndicators(true)
 	.addTo(controller)
 
 
@@ -72,7 +72,7 @@ $(document).ready(function(){
 		duration: 300 // продолжение анимации
 	})
 	.setTween(tweenGirl)
-	.addIndicators(true)
+	// .addIndicators(true)
 	.addTo(controller)
 
 
@@ -87,18 +87,54 @@ $(document).ready(function(){
 			// to make play animation once you should delete duration and add 'reverse: false'
 		})
 		.setClassToggle(this, 'fade-in')
-		.addIndicators({
-			name: 'course scene',
-			colorTrigger: 'black',
-			colorStart: 'coral',
-			colorEnd: 'pink'
-		})
+		// .addIndicators({
+		// 	name: 'course scene',
+		// 	colorTrigger: 'black',
+		// 	colorStart: 'coral',
+		// 	colorEnd: 'pink'
+		// })
 		.addTo(controller);
 	});
 
 
 
-	//animated gradient in canvas
+	//======================teachers-photo changing	
+	var imageScene1 = new ScrollMagic.Scene({
+		triggerElement: '.photo-down',
+		// duration: '130%', // use it for dissapearind at the top of the page
+		triggerHook: 0.45,    //trigger hook is in a bottom of a page (almost)
+		// reverse: false
+		// to make play animation once you should delete duration and add 'reverse: false'
+	})
+	.setClassToggle('.photo-down', 'hidden')
+	// .addIndicators({
+	// 	name: 'img-down scene',
+	// 	colorTrigger: 'black',
+	// 	colorStart: 'coral',
+	// 	colorEnd: 'pink'
+	// })
+	.addTo(controller);
+
+	var imageScene2 = new ScrollMagic.Scene({
+		triggerElement: '.photo-up',
+		// duration: '130%', // use it for dissapearind at the top of the page
+		triggerHook: 0.45,    //trigger hook is in a bottom of a page (almost)
+		// reverse: false
+		// to make play animation once you should delete duration and add 'reverse: false'
+	})
+	.setClassToggle('.photo-up', 'active')
+	// .addIndicators({
+	// 	name: 'img-down scene',
+	// 	colorTrigger: 'black',
+	// 	colorStart: 'coral',
+	// 	colorEnd: 'pink'
+	// })
+	.addTo(controller);
+	
+
+
+
+	//==================animated gradient in canvas
 	var granimInstance = new Granim({
     element: '#canvas-basic',
 	    name: 'basic-gradient',
@@ -127,113 +163,159 @@ $(document).ready(function(){
 
 
 
+	// =================Some random color-ball dancing around teachers
 
-	// ParticlesJS Config.
-	particlesJS("particles-js", {
-		particles: {
-			number: {
-				value: 80,
-				density: {
-					enable: true,
-					value_area: 700
-				}
-			},
-			color: {
-				value: "#000000"
-			},
-			shape: {
-				type: "circle",
-				stroke: {
-					width: 0,
-					color: "#eeeeee"
-				},
-				polygon: {
-					nb_sides: 5
-				}
-			},
-			opacity: {
-				value: 0.5,
-				random: false,
-				anim: {
-					enable: false,
-					speed: 1,
-					opacity_min: 0.1,
-					sync: false
-				}
-			},
-			size: {
-				value: 2,
-				random: true,
-				anim: {
-					enable: false,
-					speed: 40,
-					size_min: 0.1,
-					sync: false
-				}
-			},
-			line_linked: {
-				enable: true,
-				distance: 150,
-				color: "#00356B", //line color setted here
-				opacity: 0.7,
-				width: 1
-			},
-			move: {
-				enable: true,
-				speed: 4,
-				direction: "none",
-				random: false,
-				straight: false,
-				out_mode: "out",
-				bounce: false,
-				attract: {
-					enable: false,
-					rotateX: 600,
-					rotateY: 1200
-				}
-			}
-		},
-		interactivity: {
-			detect_on: "canvas",
-			events: {
-				onhover: {
-					enable: true,
-					mode: "grab"
-				},
-				onclick: {
-					enable: true,
-					mode: "push"
-				},
-				resize: true
-			},
-			modes: {
-				grab: {
-					distance: 140,
-					line_linked: {
-						opacity: 1
-					}
-				},
-				bubble: {
-					distance: 400,
-					size: 40,
-					duration: 2,
-					opacity: 8,
-					speed: 3
-				},
-				repulse: {
-					distance: 200,
-					duration: 0.4
-				},
-				push: {
-					particles_nb: 4
-				},
-				remove: {
-					particles_nb: 2
-				}
-			}
-		},
-		retina_detect: true
+	const colors = ["#3CC157", "#2AA7FF", "#A21FDF", "#FCBC0F", "#F85F36"];
+	const numBalls = 50;
+	const balls = [];
+	const ballContainer = document.querySelector('.teachers .balls');
+
+	for (let i = 0; i < numBalls; i++) {
+	  let ball = document.createElement("div");
+	  ball.classList.add("ball");
+	  ball.style.background = colors[Math.floor(Math.random() * colors.length)];
+	  ball.style.left = `${Math.floor(Math.random() * 80)}%`;
+	  ball.style.top = `${Math.floor(Math.random() * 80)}%`;
+	  ball.style.transform = `scale(${Math.random()})`;
+	  ball.style.width = `${Math.random()}em`;
+	  ball.style.height = ball.style.width;
+	  
+	  balls.push(ball);
+	  ballContainer.append(ball);
+	}
+
+	// Keyframes
+	balls.forEach((el, i, ra) => {
+	  let to = {
+	    x: Math.random() * (i % 2 === 0 ? -11 : 11),
+	    y: Math.random() * 12
+	  };
+
+	  let anim = el.animate(
+	    [
+	      { transform: "translate(0, 0)" },
+	      { transform: `translate(${to.x}rem, ${to.y}rem)` }
+	    ],
+	    {
+	      duration: (Math.random() + 1) * 2000, // random duration
+	      direction: "alternate",
+	      fill: "both",
+	      iterations: Infinity,
+	      easing: "ease-in-out"
+	    }
+	  );
 	});
+
+
+
+
+
+	// // ParticlesJS Config. -==================== ЛЕТАЮЩИЕ КОЗЯВКИ (ОЧЕНЬ БЪЁТ ПО ПРОИЗВОДИТЕЛЬНОСТИ)
+	// particlesJS("particles-js", {
+	// 	particles: {
+	// 		number: {
+	// 			value: 80,
+	// 			density: {
+	// 				enable: true,
+	// 				value_area: 700
+	// 			}
+	// 		},
+	// 		color: {
+	// 			value: "#000000"
+	// 		},
+	// 		shape: {
+	// 			type: "circle",
+	// 			stroke: {
+	// 				width: 0,
+	// 				color: "#eeeeee"
+	// 			},
+	// 			polygon: {
+	// 				nb_sides: 5
+	// 			}
+	// 		},
+	// 		opacity: {
+	// 			value: 0.5,
+	// 			random: false,
+	// 			anim: {
+	// 				enable: false,
+	// 				speed: 1,
+	// 				opacity_min: 0.1,
+	// 				sync: false
+	// 			}
+	// 		},
+	// 		size: {
+	// 			value: 2,
+	// 			random: true,
+	// 			anim: {
+	// 				enable: false,
+	// 				speed: 40,
+	// 				size_min: 0.1,
+	// 				sync: false
+	// 			}
+	// 		},
+	// 		line_linked: {
+	// 			enable: true,
+	// 			distance: 150,
+	// 			color: "#00356B", //line color setted here
+	// 			opacity: 0.7,
+	// 			width: 1
+	// 		},
+	// 		move: {
+	// 			enable: true,
+	// 			speed: 4,
+	// 			direction: "none",
+	// 			random: false,
+	// 			straight: false,
+	// 			out_mode: "out",
+	// 			bounce: false,
+	// 			attract: {
+	// 				enable: false,
+	// 				rotateX: 600,
+	// 				rotateY: 1200
+	// 			}
+	// 		}
+	// 	},
+	// 	interactivity: {
+	// 		detect_on: "canvas",
+	// 		events: {
+	// 			onhover: {
+	// 				enable: true,
+	// 				mode: "grab"
+	// 			},
+	// 			onclick: {
+	// 				enable: true,
+	// 				mode: "push"
+	// 			},
+	// 			resize: true
+	// 		},
+	// 		modes: {
+	// 			grab: {
+	// 				distance: 140,
+	// 				line_linked: {
+	// 					opacity: 1
+	// 				}
+	// 			},
+	// 			bubble: {
+	// 				distance: 400,
+	// 				size: 40,
+	// 				duration: 2,
+	// 				opacity: 8,
+	// 				speed: 3
+	// 			},
+	// 			repulse: {
+	// 				distance: 200,
+	// 				duration: 0.4
+	// 			},
+	// 			push: {
+	// 				particles_nb: 4
+	// 			},
+	// 			remove: {
+	// 				particles_nb: 2
+	// 			}
+	// 		}
+	// 	},
+	// 	retina_detect: true
+	// });
 
 
 
